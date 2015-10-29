4a669f2ecff412f2a8dc6f41e83dad75a22677d9
Tristan Bell <tristan@calvinonoir.com>
2015-10-29 10:55:18 +0000

    Added menus

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+	public Canvas startMenu;
+	public Canvas gameoverMenu;
+
+	// Use this for initialization
+	void Start () {
+		startMenu = startMenu.GetComponent<Canvas> ();
+		gameoverMenu = gameoverMenu.GetComponent<Canvas> ();
+		startMenu.enabled = true;
+		gameoverMenu.enabled = false;
+	}
+
+	public void StartPressed () {
+		startMenu.enabled = false;
+	}
+
+	public void RestartPressed() {
+		gameoverMenu.enabled = false;
+	}
+
+	public void ShowGameOverMenu() {
+		gameoverMenu.enabled = true;
+	}
+}
