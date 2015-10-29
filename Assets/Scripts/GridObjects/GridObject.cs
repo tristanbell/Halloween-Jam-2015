0c185cf08b76965495d125482a676bf0bafac650
Tristan Bell <tristan@calvinonoir.com>
2015-10-29 14:56:05 +0000

    Background

--- Assets/Scripts/GridObjects/GridObject.cs
+++ Assets/Scripts/GridObjects/GridObject.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class GridObject : MonoBehaviour
 {
-    protected MovementComponent m_pMovementComponent = new MovementComponent();
+    protected MovementComponent m_pMovementComponent;
 
 	// Use this for initialization
 	void Start () 
     {
+		m_pMovementComponent = GetComponent<MovementComponent> ();
 	}
 
     public MovementComponent GetMovementComponent()
--- Assets/Scripts/GridObjects/SurvivorScript.cs
+++ Assets/Scripts/GridObjects/SurvivorScript.cs
@@ -27,28 +27,35 @@ public class SurvivorScript : GridObject
     // Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		m_pMovementComponent = GetComponent<MovementComponent> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+		float speed = 0.01f;
 		switch (m_pMovementComponent.GetDirection ()) {
 		case EDirection.DOWN:
 			animator.SetInteger("Direction", 0);
+			transform.localPosition = transform.localPosition + new Vector3(0, -speed, 0);
 			break;
 		case EDirection.LEFT:
 			animator.SetInteger("Direction", 1);
+			transform.Translate(-speed, 0, 0);
 			break;
 		case EDirection.UP:
 			animator.SetInteger("Direction", 2);
+			transform.Translate(0, speed, 0);
 			break;
 		case EDirection.RIGHT:
 			animator.SetInteger("Direction", 3);
+			transform.Translate(speed, 0, 0);
 			break;
 		}
 
         // On Movement
         m_pMovementComponent.DoMovement();
+
         if (survivorFront)
         {
             //m_vNextPosition = survivorFront.GetGridPosition();
@@ -58,5 +65,17 @@ public class SurvivorScript : GridObject
             
         }
         
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Survivor") {
+			GameObject link = survivorBack;
+
+			while(link != null) {
+
+			}
+
+			link = coll.gameObject;
+		}
 	}
 }
